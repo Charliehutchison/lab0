d504e208c2551ef40a29008e18557d063c2b4c75
Charliehutchison <cmhp2024@mymail.pomona.edu>
2026-01-21 15:39:45 -0800

    First attempt at a submission for lab0

--- src/Employee.java
+++ src/Employee.java
@@ -1,10 +1,41 @@
-package settingup;
+
 
 /***
  * Class that represents an employee in a company
  */
 
 public class Employee {
+    private String name;
+    private int id;
+    private int age;
+
+    //employee counter
+    private static int employeeCounter = 0;
+
+    //Constructor
+    Employee(String name, int age){
+        this.name = name;
+        this.age = age;
+
+        this.id = employeeCounter;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public String toString(){
+        return "Employee information => {" + "name = " + name + " age = " + age + " id = " + id + "}";
+    }
+
+    public int getID(){
+        return id;
+    }
+
 
     // TODO:
     // - declare the following instance variables: name, id, age
@@ -24,16 +55,20 @@ public class Employee {
 
     public static void main(String args[]) {
         // TODO: create an object of type Employee using the constructor you created
+        Employee e1 = new Employee("Charlie", 19);
 
         // TODO: choose the instance variable you created a getter and setter for and print its contents either directly or using the getter
+        System.out.println(e1.getName());
 
         // TODO: now call the setter and change the value of the instance variable to something else
+        e1.setName("Gavin");
 
         // TODO: confirm that the change took place by printing it again
-
+        System.out.println(e1.getName());
         // TODO: print the object (it should implicitly call the toString method you overrode)
+        System.out.println(e1);
 
         // TODO: Try at least one more thing :) Leave a comment about what you did
-
+        System.out.println(e1.getID());
     }
 }
