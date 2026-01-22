1a5095f0ecfa3743748fc327a47cb0c2b460990a
Charliehutchison <cmhp2024@mymail.pomona.edu>
2026-01-21 16:03:38 -0800

    Second attempt at submitting lab0

--- src/Employee.java
+++ src/Employee.java
@@ -37,35 +37,19 @@ public class Employee {
     }
 
 
-    // TODO:
-    // - declare the following instance variables: name, id, age
-    // - what types should you use for each? (https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html)
-    // - what access modifiers? (https://docs.oracle.com/javase/tutorial/java/javaOO/accesscontrol.html)
 
-    // TODO: declare a static variable `employeeCounter` as a counter of Employee objects
-
-    // TODO: add a constructor that takes a name and age as parameters and initializes all instance variables
-    // https://www.geeksforgeeks.org/java/constructors-in-java/
-
-    // TODO: create getter and setter methods for name and id
-    // https://www.w3schools.com/java/java_encapsulation.asp
-
-    // TODO: override the toString method. Be careful when you write its signature
-    // https://www.geeksforgeeks.org/java/object-tostring-method-in-java/
 
     public static void main(String args[]) {
-        // TODO: create an object of type Employee using the constructor you created
+        
         Employee e1 = new Employee("Charlie", 19);
 
-        // TODO: choose the instance variable you created a getter and setter for and print its contents either directly or using the getter
         System.out.println(e1.getName());
 
-        // TODO: now call the setter and change the value of the instance variable to something else
+        
         e1.setName("Gavin");
 
-        // TODO: confirm that the change took place by printing it again
         System.out.println(e1.getName());
-        // TODO: print the object (it should implicitly call the toString method you overrode)
+       
         System.out.println(e1);
 
         // I created another method called getID() similar to get name and then printed it out.
